13b08b0057d6d35859d2107c32011deefdfc9ab3
fujimotok <fujimotok@users.noreply.github.com>
2024-12-31 22:47:17 +0900

    検索機のを実装

--- ToolWindow1Data.cs
+++ ToolWindow1Data.cs
@@ -16,21 +16,30 @@ namespace Imenu
         public ToolWindow1Data()
         {
             this.JumpCommand = new AsyncCommand(this.JumpExecuteAsync);
+            this._timer = new Timer((x) => { this.FilterItems(); }, null, 500, Timeout.Infinite);
 
-            this.ImenuItems.Add(new ImenuItem() { Name = "item1", Line = 1 });
-            this.ImenuItems.Add(new ImenuItem() { Name = "item1", Line = 2 });
-            this.ImenuItems.Add(new ImenuItem() { Name = "item1", Line = 3 });
-            this.ImenuItems.Add(new ImenuItem() { Name = "item1", Line = 4 });
-            this.ImenuItems.Add(new ImenuItem() { Name = "item1", Line = 5 });
+            this._dataSouce.Add(new ImenuItem() { Name = "hello", Line = 1 });
+            this._dataSouce.Add(new ImenuItem() { Name = "world", Line = 2 });
+            this._dataSouce.Add(new ImenuItem() { Name = "hello world", Line = 3 });
+
+            this.ImenuItems = this._dataSouce;
         }
+
+        private Timer _timer;
 
         private string _text = string.Empty;
         [DataMember]
         public string Text
         {
             get => _text;
-            set => SetProperty(ref this._text, value);
+            set
+            {
+                SetProperty(ref this._text, value);
+                this._timer.Change(300, Timeout.Infinite);
+            }
         }
+
+        private ObservableCollection<ImenuItem> _dataSouce = new ObservableCollection<ImenuItem>();
 
         private ImenuItem? _selectedItem = null;
         [DataMember]
@@ -56,6 +65,22 @@ namespace Imenu
             MessageBox.Show($"hello! {SelectedItem?.Line ?? 0}");
 
             return Task.CompletedTask;
+        }
+
+        private void FilterItems()
+        {
+            if (string.IsNullOrEmpty(this.Text))
+            {
+                this.ImenuItems = this._dataSouce;
+                return;
+            }
+
+            var words = this.Text.Split(' ');
+
+            // 検索文字列がすべて含まれるアイテムをフィルタリング
+            var filteredItems = this._dataSouce.Where(item => words.All(word => item.Name.Contains(word)));
+
+            this.ImenuItems = new ObservableCollection<ImenuItem>(filteredItems.ToList());
         }
     }
 }
